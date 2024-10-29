5e0b59fde1f29cb73b06492a57e1dc02b7421b9e
ssweilee <119922122+ssweilee@users.noreply.github.com>
2024-10-29 23:44:07 +0000

    Update

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import NavBar from './components/NavBar'
+import About from './components/About'
+import Skills from './components/Skills'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import { useState, createContext } from "react"
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export const ThemeContext = createContext(null)
+
+function App() {
+  const [ theme, setTheme ] = useState('dark')
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "dark" ? "light" : "dark"))
+  }
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id={theme}>
+        <NavBar />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
+    </ThemeContext.Provider>
+  )
+}
+
+export default App
