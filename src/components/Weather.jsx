99de1d20c291f2b11ab405adbdfe77fa01578f6f
ssweilee <119922122+ssweilee@users.noreply.github.com>
2025-06-07 21:06:37 +0100

    fixed weather fetching

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -11,7 +11,9 @@ function Weather () {
         const getWeather = async () => {
           try {
             const position = await new Promise((resolve, reject) =>
-              navigator.geolocation.getCurrentPosition(resolve, reject)
+              navigator.geolocation.getCurrentPosition(resolve, reject, {
+                timeout: 15000, 
+              })
             );
             const lat = position.coords.latitude;
             const lon = position.coords.longitude;
