ae4bc8b578d8754a7a838e55aa4e91e11d1f8c93
ssweilee <119922122+ssweilee@users.noreply.github.com>
2025-09-27 00:23:50 +0800

    rebranding skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCss3Alt, faGitAlt, faGithub, faHtml5, faJs, faReact, faBootstrap, faMailchimp, faNode, faJava} from '@fortawesome/free-brands-svg-icons'
-import { faSpinner, faTerminal, faC, faDatabase } from '@fortawesome/free-solid-svg-icons'
+import { faCss3Alt, faGitAlt, faGithub, faHtml5, faJs, faReact, faBootstrap, faMailchimp, faNode, faJava, faPython} from '@fortawesome/free-brands-svg-icons'
+import { faBook, faC, faDatabase } from '@fortawesome/free-solid-svg-icons'
 //import 'animate.css'
 import TrackVisibility from 'react-on-screen'
 import { Container } from 'react-bootstrap'
@@ -10,78 +10,81 @@ import { links } from '../constants/links'
 
 
 function Skills () {
+    const languages = [
+        { icon: faJava, label: 'Java' },
+        { icon: faJs, label: 'JavaScript' },
+        { icon: faPython, label: 'Python' },
+        { icon: faC, label: 'C' },
+        { icon: faDatabase, label: 'SQL' },
+        { icon: faHtml5, label: 'HTML5' },
+        { icon: faCss3Alt, label: 'CSS3' },
+    ];
+
+    const frameworks = [
+        { icon: faReact, label: 'React' },
+        { icon: faNode, label: 'Node.js' },
+        { img: express, label: 'Express.js' },
+        { img: flutter, label: 'Flutter' },
+        { icon: faGitAlt, label: 'Git' },
+        { icon: faGithub, label: 'GitHub' },
+    ];
+
+    const spokenLanguages = [
+        { icon: faBook, label: 'English' },
+        { icon: faBook, label: 'Mandarin' },
+        { icon: faBook, label: 'Basic French' },
+        { icon: faBook, label: 'Basic Korean' },
+      ];
+
     return (
         <section className='skill' id='skills'>
             <Container>
-                <div className="row">
-                    <TrackVisibility>
-                    {({ isVisible }) =>
-                        <div>
-                            <div className="skill-bx">
-                                <h2>Skills</h2>
-                                <br></br>
-                                <div className="skills-container">
-                                    <div className='skill-div'>
-                                        <FontAwesomeIcon className="skill-icon" icon={faJava} />
-                                        <p>JAVA</p>
+                <TrackVisibility>
+                {({ isVisible }) => (
+                    <div className="skill-bx">
+                        <h2>Skills</h2>
+                        <br></br>
+                        <div className="skills-wrapper">
+                            <div className="skills-column languages">
+                                <h3>Programming Languages</h3>
+                                {languages.map((skill, idx) => (
+                                    <div key={idx} className='skill-div'>
+                                        {skill.icon && <FontAwesomeIcon className="skill-icon" icon={skill.icon} />}
+                                        {skill.img && <img className="skill-icon svg-icon" src={skill.img} alt={skill.label} />}
+                                        <p>{skill.label}</p>
                                     </div>
-                                    <div className='skill-div'>
-                                        <FontAwesomeIcon className="skill-icon javascript" icon={faJs} />
-                                        <p>JAVASCRIPT</p>
-                                    </div>
-                                    <div className='skill-div'>
-                                        <FontAwesomeIcon className="skill-icon" icon={faDatabase} />
-                                        <p>SQL</p>
-                                    </div>
-                                    <div className='skill-div'>
-                                        <FontAwesomeIcon className="skill-icon" icon={faC} />
-                                        <p>C</p>
-                                    </div>
-                                    <div className='skill-div'>
-                                        <FontAwesomeIcon className="skill-icon" icon={faNode} />
-                                        <p>Node.js</p>
-                                    </div>
-                                    <div className='skill-div'>
-                                    <img className="svg-icon" src={express} alt="Express.js" />
-                                        <p>Express.js</p>
-                                    </div>
-                                    <div className='skill-div'>
-                                        <FontAwesomeIcon className="skill-icon" icon={faHtml5} />
-                                        <p>HTML5</p>
-                                    </div>
-                                    <div className='skill-div'>
-                                        <FontAwesomeIcon className="skill-icon" icon={faCss3Alt} />
-                                        <p>CSS</p>
-                                    </div>
-                                    <div className='skill-div'>
-                                        <FontAwesomeIcon className="skill-icon react" icon={faReact} />
-                                        <p>REACT</p>
-                                    </div>
-                                    <div className='skill-div'>
-                                        <img src={flutter} alt='Flutter' className='skill-icon svg-icon' />
-                                        <p>Flutter</p>
-                                    </div>
-                                    <div className='skill-div'>
-                                        <FontAwesomeIcon className="skill-icon" icon={faGitAlt} />
-                                        <p>GIT</p>
-                                    </div>
-                                    <div className='skill-div'>
-                                        <FontAwesomeIcon className="skill-icon" icon={faGithub} />
-                                        <p>GITHUB</p>
-                                    </div>
+                                ))}
+                            </div>
+                            <div className="skills-column frameworks">
+                                    <h3>Frameworks & Tools</h3>
+                                    {frameworks.map((skill, idx) => (
+                                        <div key={idx} className='skill-div'>
+                                            {skill.icon && <FontAwesomeIcon className="skill-icon" icon={skill.icon} />}
+                                            {skill.img && <img className={`skill-icon svg-icon ${skill.label === 'Express.js' ? 'express-icon' : skill.label === 'Flutter' ? 'flutter-icon' : ''}`} src={skill.img} alt={skill.label} />}
+                                            <p>{skill.label}</p>
+                                        </div>
+                                    ))}
                                 </div>
-                                <div className="resume-link-wrapper">
-                                    <p className="resume-link">Checkout more on my <a href={links.resume} target="_blank" rel="noopener noreferrer">Resume</a></p>
+
+                                <div className="skills-column spokenLanguages">
+                                    <h3>Languages</h3>
+                                    {spokenLanguages.map((lang, idx) => (
+                                    <div key={idx} className='skill-div'>
+                                        {lang.icon && <FontAwesomeIcon className="skill-icon" icon={lang.icon} />}
+                                        <p>{lang.label}</p>
+                                    </div>
+                                    ))}
                                 </div>
                             </div>
-                        </div>}
-                    </TrackVisibility>
-                </div>
-            </Container>
-                    
-        </section>
-        
-    )
+                        <div className="resume-link-wrapper">
+                            <p className="resume-link">Checkout more on my <a href={links.resume} target="_blank" rel="noopener noreferrer">Resume</a></p>
+                        </div>
+                    </div>
+                )}
+                </TrackVisibility>
+            </Container>         
+        </section>  
+    );
 }
 
 export default Skills
