2c4a3811b8992682fcd65991ba28cdee56edae7e
ssweilee <119922122+ssweilee@users.noreply.github.com>
2025-09-28 03:15:49 +0800

    midified RSW

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,6 @@ import { ThemeContext } from '../App'
 import ReactSwitch from 'react-switch'
 import Weather from './Weather'
 import { HashLink } from 'react-router-hash-link'
-
 import { links } from '../constants/links'
 
 function NavBar() {
@@ -21,9 +20,7 @@ function NavBar() {
       const handleScroll = () => {
             setScrolled(window.scrollY > 50)
       }
-  
       window.addEventListener("scroll", handleScroll)
-
       return () => window.removeEventListener("scroll", handleScroll)
   }, [])
   
@@ -38,7 +35,6 @@ function NavBar() {
   const handleNavClick = (id) => {
     setActiveLink(id)
     setCollapsingOut(true)
-    // 先收回 menu
     setMenuExpanded(false)
     setTimeout(() => {
       setCollapsingOut(false)
@@ -58,57 +54,48 @@ function NavBar() {
     const navbarClass = `${scrolled ? "scrolled " : ""}${theme === "dark" ? "dark" : "light"}`
 
   return (
-
-      <Navbar expand="md" className={navbarClass} expanded={menuExpanded} onToggle={setMenuExpanded} >
-        <Container className="navbar-container">
-        <div className="mobile-header">
-          <Navbar.Toggle aria-controls="basic-navbar-nav"  />
-          <span className='navbar-text'>
-            <div className='social-icons'>
-              <a href={links.linkedin} target="_blank"><FontAwesomeIcon className="social-icon" icon={faLinkedinIn} /></a>
-              <a href={links.mail} target="_blank"><FontAwesomeIcon className="social-icon" icon={faEnvelope} /></a>
-              <a href={links.github} target="_blank"><FontAwesomeIcon className="social-icon" icon={faGithub} /></a>
-            </div>
-            <HashLink to='#connect'>
-              <button><span>Let&apos;s Connect</span></button>
-            </HashLink>
-          </span>
-        </div>
-          
-          <Navbar.Collapse id="basic-navbar-nav" className={collapsingOut ? 'collapsing-out' : ''} expanded={menuExpanded}>
-            <Nav className="me-auto">
-            <Nav.Link as={HashLink} smooth to="#about" onClick={() => handleNavClick('about')} className={activeLink==='about' ? 'active navbar-link':'navbar-link'}>About</Nav.Link>
-<Nav.Link as={HashLink} smooth to="#skills" onClick={() => handleNavClick('skills')} className={activeLink==='skills' ? 'active navbar-link':'navbar-link'}>Skills</Nav.Link>
-<Nav.Link as={HashLink} smooth to="#projects" onClick={() => handleNavClick('projects')} className={activeLink==='projects' ? 'active navbar-link':'navbar-link'}>Projects</Nav.Link>
-
-
-
-
-
-
-            </Nav>
-            <div className="navbar-tools">
-              <Weather />
-              <div className="switch">
-                  <ReactSwitch className="custom-switch" onChange={toggleTheme} checked={theme === "dark"} uncheckedIcon={<FontAwesomeIcon className="moon-icon" icon={faMoon} />} checkedIcon={<FontAwesomeIcon className="sun-icon" icon={faSun} />} />
-                </div>
-              </div>
-          </Navbar.Collapse>
-          <div className="non-mobile-navbar-text">
-            <span className='navbar-text'>
-              <div className='social-icons'>
-                <a href={links.linkedin} target="_blank"><FontAwesomeIcon className="social-icon" icon={faLinkedinIn} /></a>
-                <a href={links.mail} target="_blank"><FontAwesomeIcon className="social-icon" icon={faEnvelope} /></a>
-                <a href={links.github} target="_blank"><FontAwesomeIcon className="social-icon" icon={faGithub} /></a>
-              </div>
-              <HashLink to='#connect'>
-                <button><span>Let&apos;s Connect</span></button>
-              </HashLink>
-            </span>
+    <Navbar expand="md" className={navbarClass} expanded={menuExpanded} onToggle={setMenuExpanded} >
+      <Container className="navbar-container">
+      <div className="mobile-header">
+        <Navbar.Toggle aria-controls="basic-navbar-nav"  />
+        <span className='navbar-text'>
+          <div className='social-icons'>
+            <a href={links.linkedin} target="_blank"><FontAwesomeIcon className="social-icon" icon={faLinkedinIn} /></a>
+            <a href={links.mail} target="_blank"><FontAwesomeIcon className="social-icon" icon={faEnvelope} /></a>
+            <a href={links.github} target="_blank"><FontAwesomeIcon className="social-icon" icon={faGithub} /></a>
           </div>
-        </Container>
-      </Navbar>
-
+          <HashLink to='#connect'>
+            <button><span>Let&apos;s Connect</span></button>
+          </HashLink>
+        </span>
+      </div>
+      <Navbar.Collapse id="basic-navbar-nav" className={collapsingOut ? 'collapsing-out' : ''} expanded={menuExpanded}>
+        <Nav className="me-auto">
+        <Nav.Link as={HashLink} smooth to="#about" onClick={() => handleNavClick('about')} className={activeLink==='about' ? 'active navbar-link':'navbar-link'}>About</Nav.Link>
+        <Nav.Link as={HashLink} smooth to="#skills" onClick={() => handleNavClick('skills')} className={activeLink==='skills' ? 'active navbar-link':'navbar-link'}>Skills</Nav.Link>
+        <Nav.Link as={HashLink} smooth to="#projects" onClick={() => handleNavClick('projects')} className={activeLink==='projects' ? 'active navbar-link':'navbar-link'}>Projects</Nav.Link>
+        </Nav>
+        <div className="navbar-tools">
+          <Weather />
+          <div className="switch">
+              <ReactSwitch className="custom-switch" onChange={toggleTheme} checked={theme === "dark"} uncheckedIcon={<FontAwesomeIcon className="moon-icon" icon={faMoon} />} checkedIcon={<FontAwesomeIcon className="sun-icon" icon={faSun} />} />
+            </div>
+          </div>
+      </Navbar.Collapse>
+      <div className="non-mobile-navbar-text">
+        <span className='navbar-text'>
+          <div className='social-icons'>
+            <a href={links.linkedin} target="_blank"><FontAwesomeIcon className="social-icon" icon={faLinkedinIn} /></a>
+            <a href={links.mail} target="_blank"><FontAwesomeIcon className="social-icon" icon={faEnvelope} /></a>
+            <a href={links.github} target="_blank"><FontAwesomeIcon className="social-icon" icon={faGithub} /></a>
+          </div>
+          <HashLink to='#connect'>
+            <button><span>Let&apos;s Connect</span></button>
+          </HashLink>
+        </span>
+      </div>
+      </Container>
+    </Navbar>
   );
 }
 
